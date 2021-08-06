d0005de95c19561dae467c23076398453ce336f8
Gavle-hub <erikdaniel650@gmail.com>
2021-08-05 18:08:03 -0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-let total = document.getElementById("total");
-let people = document.getElementById("people");
-let result = document.getElementById("result");
-let tip = document.getElementById("tip-input");
+const total = document.getElementById("total");
+const people = document.getElementById("people");
+const result = document.getElementById("result");
+const tip = document.getElementById("tip-input");
+const tipTotal = document.getElementById("total-tip");
+let finalValue = 0;
+let peopleValue = 0;
+const maxValue = 99999999999999;
 
 total.addEventListener("input", calculate);
 people.addEventListener("input", calculate);
@@ -9,18 +13,51 @@ tip.addEventListener("input",calculate);
 
 
 function calculate() {
-    if(total.value != 0 && people.value != 0 && tip.value != 0){
-    let totalValue = total.value;
-    let tipValue = tip.value;
-    let peopleValue = people.value;
+    if(checkInput()){
+        
+        let totalValue = total.value;
+        let tipValue = tip.value;
+        peopleValue = people.value;
 
-    let calculation1 = totalValue * (tipValue / 100); 
-    let calculation2 = calculation1 / peopleValue;
-    let finalValue = calculation2.toFixed(2);
+        let calculation1 = totalValue * (tipValue / 100); 
+        let calculation2 = calculation1 / peopleValue;
+         finalValue = calculation2.toFixed(2);
+        result.style.visibility = "visible";
+        tipTotal.textContent =  "Total Tip: $" + calculation1.toFixed(2) ;
+        updateResult();
+    }
+    else {
+        tipTotal.textContent = "Total Tip: $0.00";
+        peopleValue = 0;
+        finalValue = 0;
+        updateResult();
 
-    result.style.visibility = "visible";
-    result.textContent = people.value + " people have to pay $" + finalValue + " dollars";
+    }
     
 }
 
+
+function checkInput() {
+    if(total.value != "" && total.value != 0 &&  
+     people.value != "" && people.value != 0 &&
+     tip.value != "" && tip.value != 0) {
+         return true;
+     }
+     else {
+         return false
+     }
+}
+
+function updateResult()  {
+    let peopleBold = document.createElement("b");
+    peopleBold.textContent = peopleValue;
+    
+    let moneyBold = document.createElement("b");
+    moneyBold.textContent = finalValue;
+    
+    let text1 = document.createTextNode(" people have to pay $");
+    result.textContent = "";
+    result.appendChild(peopleBold);
+    result.appendChild(text1);
+    result.appendChild(moneyBold)
 }
